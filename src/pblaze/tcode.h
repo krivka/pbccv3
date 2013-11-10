f9db5ac8d8167f73e6d37c0e50bd23031f5215b8
Martin Briza <m@rtinbriza.cz>
2013-11-10 01:06:08 +0100

    Added the rest of the instruction set and some other classes

--- src/pblaze/tcode.h
+++ src/pblaze/tcode.h
@@ -39,16 +39,20 @@ public:
     };
     virtual void printOperand() const = 0;
     virtual Type type() const = 0;
+    
+    class Register;
+    class Constant;
+    class Port;
 };
 
-class Register : public Operand {
+class Operand::Register : public Operand {
 public:
     enum Bank {
         BANK_A,
         BANK_B
     };
     Register(Bank bank, uint8_t no) : m_bank(bank), m_number(no), m_free(true) {}
-    inline virtual Type type() const { return Operand::Register; }
+    virtual Type type() const { return Operand::Register; }
     virtual void printOperand() const;
 private:
     bool m_bank;
@@ -56,11 +60,23 @@ private:
     bool m_free;
 };
 
+//TBD
+class Operand::Constant : public Operand {
+
+};
+
+class Operand::Port : public Operand {
+    
+};
 
 class Instruction {
 public:
-    virtual const char *name() const = 0;
-    virtual void printInstruction() const = 0;
+    virtual const char *name() const {
+        return "reimplement this!";
+    }
+    virtual void printInstruction() const {
+        fprintf(stderr, "\t%s\n", name());
+    }
 
     // The classes are in the same order as in the KCPSM manual
 protected:
@@ -77,10 +93,7 @@ public: // inheriting from TwoOperandInstruction
         class Compare;
 
 public: // direct children
-        class SL;
-        class RL;
-        class SR;
-        class RR;
+        class Shift;
         class Regbank;
         class Input;
         class Output;
@@ -115,7 +128,7 @@ private:
 /**
  * This class assumes the following things:
  * 1) Every of the subclassing instructions does have two operands, in which the first one is a register.
- *    The second one is either a register or a constant (if constantOp2() returns true).
+ *    The second one is either a register or a constant.
  * 2) Carry: The instruction is in its carry version when the class is constructed with withCarry = true.
  *    Subclassing to set only the operands results in not using the carry option at all.
  *    This basically means arithmetic operations will want the third argument but logical and move operations won't.
@@ -124,7 +137,6 @@ class Instruction::TwoOperandInstruction : public Instruction {
 public:
     explicit TwoOperandInstruction(const Operand *op1, const Operand *op2, bool withCarry = false);
     virtual void printInstruction() const;
-    inline virtual bool constantOp2() { return true; }
 protected:
     const Operand *m_op1;
     const Operand *m_op2;
@@ -135,64 +147,64 @@ class Instruction::Load : public TwoOperandInstruction {
 public:
     explicit Load(const Operand *op1, const Operand *op2)
             : TwoOperandInstruction(op1, op2) {}
-    inline virtual const char *name() const { return "LOAD"; }
+    virtual const char *name() const { return "LOAD"; }
 };
 
 class Instruction::Star : public TwoOperandInstruction {
 public:
-    explicit Star(const Operand *op1, const Operand *op2)
+    explicit Star(const Operand::Register *op1, const Operand::Register *op2)
             : TwoOperandInstruction(op1, op2) {}
-    inline virtual const char *name() const { return "STAR"; }
-    inline virtual bool constantOp2() { return false; }
+    virtual const char *name() const { return "STAR"; }
+    virtual bool constantOp2() { return false; }
 };
 
 class Instruction::And : public TwoOperandInstruction {
 public:
     explicit And(const Operand *op1, const Operand *op2)
             : TwoOperandInstruction(op1, op2) {}
-    inline virtual const char *name() const { return "AND"; }
+    virtual const char *name() const { return "AND"; }
 };
 
 class Instruction::Or : public TwoOperandInstruction {
 public:
     explicit Or(const Operand *op1, const Operand *op2)
             : TwoOperandInstruction(op1, op2) {}
-    inline virtual const char *name() const { return "OR"; }
+    virtual const char *name() const { return "OR"; }
 };
 
 class Instruction::Xor : public TwoOperandInstruction {
 public:
     explicit Xor(const Operand *op1, const Operand *op2)
             : TwoOperandInstruction(op1, op2) {}
-    inline virtual const char *name() const { return "XOR"; }
+    virtual const char *name() const { return "XOR"; }
 };
 
 class Instruction::Add : public TwoOperandInstruction {
 public:
     explicit Add(const Operand *op1, const Operand *op2, bool withCarry = false)
             : TwoOperandInstruction(op1, op2, withCarry) {}
-    inline virtual const char *name() const { return "ADD"; }
+    virtual const char *name() const { return "ADD"; }
 };
 
 class Instruction::Sub : public TwoOperandInstruction {
 public:
     explicit Sub(const Operand *op1, const Operand *op2, bool withCarry = false)
             : TwoOperandInstruction(op1, op2, withCarry) {}
-    inline virtual const char *name() const { return "SUB"; }
+    virtual const char *name() const { return "SUB"; }
 };
 
 class Instruction::Test : public TwoOperandInstruction {
 public:
     explicit Test(const Operand *op1, const Operand *op2, bool withCarry = false)
             : TwoOperandInstruction(op1, op2, withCarry) {}
-    inline virtual const char *name() const { return "TEST"; }
+    virtual const char *name() const { return "TEST"; }
 };
 
 class Instruction::Compare : public TwoOperandInstruction {
 public:
     explicit Compare(const Operand *op1, const Operand *op2, bool withCarry = false)
             : TwoOperandInstruction(op1, op2, withCarry) {}
-    inline virtual const char *name() const { return "COMPARE"; }
+    virtual const char *name() const { return "COMPARE"; }
 };
 
 class Instruction::Conditional : public Instruction {
@@ -217,25 +229,122 @@ class Instruction::Jump : public Conditional {
 public:
     explicit Jump(Conditional::Type type, const Label *op1)
             : Conditional(type, op1, NULL) {}
-    explicit Jump(const Register *op1, const Register *op2)
+    explicit Jump(const Operand::Register *op1, const Operand::Register *op2)
             : Conditional(Conditional::Indirect, op1, op2) {}
-    inline virtual const char *name() const { return "JUMP"; }
+    virtual const char *name() const { return "JUMP"; }
 };
 
 class Instruction::Call : public Conditional {
 public:
     explicit Call(Conditional::Type type, const Label *op1)
             : Conditional(type, op1, NULL) {}
-    explicit Call(const Register *op1, const Register *op2)
+    explicit Call(const Operand::Register *op1, const Operand::Register *op2)
             : Conditional(Conditional::Indirect, op1, op2) {}
-    inline virtual const char *name() const { return "CALL"; }
+    virtual const char *name() const { return "CALL"; }
 };
 
 class Instruction::Return : public Conditional {
 public:
     explicit Return(Conditional::Type type)
             : Conditional(type, NULL, NULL) {}
-    inline virtual const char *name() const { return "RETURN"; }
+    virtual const char *name() const { return "RETURN"; }
+};
+
+class Instruction::Shift : public Instruction {
+public:
+    Shift(const Operand::Register *op1, Direction dir, Style style = Fill0)
+            : Instruction(), m_op1(op1), m_direction(dir), m_style(style) {}
+    virtual const char *name() const {
+        if (m_direction == Right)
+            switch(m_style) {
+                case Fill0: return "SR0";
+                case Fill1: return "SR1";
+                case FillMSB: return "SRX";
+                case FillCarry: return "SRA";
+                case Rotate: return "RR";
+            }
+        else
+            switch(m_style) {
+                case Fill0: return "SL0";
+                case Fill1: return "SL1";
+                case FillMSB: return "SLX";
+                case FillCarry: return "SLA";
+                case Rotate: return "RL";
+            }
+    }
+    enum Direction {
+        Right,
+        Left
+    };
+    enum Style {
+        Fill0,
+        Fill1,
+        FillMSB,
+        FillCarry,
+        Rotate
+    };
+private:
+    const Operand::Register *m_op1;
+    Direction m_direction;
+    Style m_style;
+};
+
+class Instruction::Input : public TwoOperandInstruction {
+public:
+    explicit Input(const Operand::Register *op1, const Operand *op2)
+            : TwoOperandInstruction(op1, op2) {}
+    virtual const char *name() const { return "INPUT"; }
+};
+
+class Instruction::Output : public TwoOperandInstruction {
+public:
+    explicit Output(const Operand::Register *op1, const Operand *op2)
+            : TwoOperandInstruction(op1, op2) {}
+    explicit Output(const Operand::Constant *op1, const Operand::Port *op2)
+            : TwoOperandInstruction(op1, op2) {}
+    virtual const char *name() const { return m_op2->type() == Operand::Port4b ? "OUTPUTK" : "OUTPUT"; }
+};
+
+class Instruction::Store : public TwoOperandInstruction {
+public:
+    explicit Store(const Operand *op1, const Operand *op2)
+            : TwoOperandInstruction(op1, op2) {}
+    virtual const char *name() const { return "STORE"; }
+};
+
+class Instruction::Fetch : public TwoOperandInstruction {
+    explicit Fetch(const Operand *op1, const Operand *op2)
+            : TwoOperandInstruction(op1, op2) {}
+    virtual const char *name() const { return "FETCH"; }
+};
+
+class Instruction::ReturnI : public Instruction {
+public:
+    explicit ReturnI(bool state)
+            : Instruction(), m_state(state) {}
+    virtual const char *name() const { return m_state ? "RETURNI ENABLE" : "RETURNI DISABLE"; }
+private:
+    bool m_state;
+};
+
+class Instruction::Interrupt : public Instruction {
+public:
+    explicit Interrupt(bool state)
+            : Instruction(), m_state(state) {}
+    virtual const char *name() const { return m_state ? "ENABLE INTERRUPT" : "DISABLE INTERRUPT"; }
+private:
+    bool m_state;
+};
+
+class Instruction::Hwbuild : public Instruction {
+public:
+    explicit Hwbuild(const Operand::Register *op1)
+            : Instruction(), m_op1(op1) {}
+    virtual const char *name() const { return "HWBUILD"; }
+private:
+    const Operand::Register *m_op1;
 };
 
 #endif
+
+struct C;
