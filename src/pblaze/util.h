916f6f40165ce317285a162411eb8e44ec673591
Martin Briza <m@rtinbriza.cz>
2014-03-23 03:20:02 +0100

    These macros will be usable more and more often

--- src/pblaze/util.h
+++ src/pblaze/util.h
@@ -2,6 +2,9 @@
 #define PBLAZE_UTIL_H
 #ifdef __cplusplus
 
+#define CRASH() do { *(int*)NULL=0; } while(0)
+#define CRASH_IF(x) do { if (x) CRASH() } while(0)
+
 class Emitter {
 public:
     Emitter& operator<<(const char *s);
