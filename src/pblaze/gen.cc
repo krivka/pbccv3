2a014e17f2c625dc7faa336ef3b70d0ea8887524
Martin Briza <m@rtinbriza.cz>
2014-04-06 00:12:54 +0200

    Get rid of some unused code

--- src/pblaze/gen.cc
+++ src/pblaze/gen.cc
@@ -25,26 +25,6 @@ void GoTo(ICode *ic) {
     emit << I::Jump(ic->getLabel());
 }
 
-void AssignLiteral(ICode *ic) {
-    Operand *result = ic->getResult();
-    Operand *right = ic->getRight();
-
-    if (!right->isLiteral)
-        return;
-
-    int size = right->getType()->getSize();
-
-
-
-    auto getByte = [](unsigned long n, uint8_t byte) {
-        return (n & (0xFF << (byte * 8))) >> byte * 8;
-    };
-
-    for (Emitter::i = 0; Emitter::i < result->getType()->getSize(); Emitter::i++) {
-        emit << I::Load(result, getByte(right->getValue()->getUnsignedLong(), Emitter::i));
-    }
-}
-
 void Assign(ICode *ic) {
     Operand *result = ic->getResult();
     Operand *right = ic->getRight();
@@ -90,14 +70,21 @@ void Sub(ICode *ic) {
     }
 }
 
+void Ifx(ICode *ic) {
+
+}
+
 std::map<unsigned int, genFunc> map {
     { FUNCTION, Function },
     { LABEL, Label },
     { CALL, Call },
     { GOTO, GoTo },
+
     { '=', Assign },
     { '+', Add },
     { '-', Sub },
+
+    { IFX, Ifx },
 };
 
 };
@@ -108,8 +95,6 @@ void genPBlazeCode(ICode *lic) {
     for (ic = lic; ic; ic = ic->getNext()) {
         if (ic->generated)
             continue;
-
-
 
         Gen::genFunc gen = Gen::map[ic->op];
         if (gen)
