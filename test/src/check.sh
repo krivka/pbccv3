bdd46122b507b43037b8ce59a15c947e65332829
Martin Briza <m@rtinbriza.cz>
2014-05-01 23:11:58 +0200

    Some C++ magic for testing

--- test/src/check.sh
+++ test/src/check.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 CPP="g++"
+CPPFLAGS="-std=c++11 -O0"
 TESTSRC="main.cc"
 TEMPBINARY="temp"
 ASM="./tools/asm"
@@ -12,7 +13,7 @@ function test() {
     ${SDCC} -mpblaze "$1" -o "${1/.c/}"
     ${ASM} -l -6 "${1/.c/.psm}" 2>"${1/.c/.err}"
     ${SIM} "${1/.c/.psm}" > "${1/.c/.trl}" 2>>"${1/.c/.err}"
-    ${CPP} -DTESTFILE="\"${1/.c/.tst}\"" -DTESTRESULT="\"${1/.c/.trl}\"" "${TESTSRC}" -o ${TEMPBINARY} 2>>${1/.c/.err}
+    ${CPP} ${CPPFLAGS} -DTESTFILE="\"${1/.c/.tst}\"" -DTESTRESULT="\"${1/.c/.trl}\"" "${TESTSRC}" -o ${TEMPBINARY} 2>>${1/.c/.err}
     (./${TEMPBINARY} 2>/dev/null && echo "${1/.c/}" "pass") || echo "${1/.c/}" "fail"
     rm -f temp "${1/.c/.trl}" "${1/.c/.psm}" "${1/.c/.err}" 2>/dev/null
 }
