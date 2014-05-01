e0a8eaf679076382e698bff9365815439d946df6
Martin Briza <m@rtinbriza.cz>
2014-05-01 22:44:08 +0200

    Properly format the output for tests

--- test/src/check.sh
+++ test/src/check.sh
@@ -9,12 +9,12 @@ SDCC="../../bin/sdcc"
 
 function test() {
     rm -f temp "${1/.c/.trl}"
-    ${SDCC} -mpblaze "$1"
+    ${SDCC} -mpblaze "$1" -o "${1/.c/}"
     ${ASM} -l -6 "${1/.c/.psm}" 2>"${1/.c/.err}"
     ${SIM} "${1/.c/.psm}" > "${1/.c/.trl}" 2>>"${1/.c/.err}"
     ${CPP} -DTESTFILE="\"${1/.c/.tst}\"" -DTESTRESULT="\"${1/.c/.trl}\"" "${TESTSRC}" -o ${TEMPBINARY} 2>>${1/.c/.err}
     (./${TEMPBINARY} 2>/dev/null && echo "${1/.c/}" "pass") || echo "${1/.c/}" "fail"
-    rm -f temp "${1/.c/.trl}" 2>/dev/null
+    rm -f temp "${1/.c/.trl}" "${1/.c/.psm}" "${1/.c/.err}" 2>/dev/null
 }
 
 if [ -z $1 ]; then
